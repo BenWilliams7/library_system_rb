6633663db339a7547e6d34cac211dcd9491b87b0
Ben Williams and Grady Shelton <grady@>
2017-05-03 11:42:23 -0700

    add index patron and book list pages to sinatra

--- app.rb
+++ app.rb
@@ -1,10 +1,20 @@
 require 'sinatra'
 require 'sinatra/reloader'
-# require './lib/library_system'
+require './lib/book'
+require './lib/author'
+require './lib/patron'
 require 'pry'
 
 also_reload('lib/**/*.rb')
 
 get('/') do
   erb(:index)
+end
+
+get('/patron_list') do
+  erb(:patron_list)
+end
+
+get('/book_list') do
+  erb(:book_list)
 end
