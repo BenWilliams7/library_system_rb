40eb99ac90634663de61cfaa850b4217812d9d91
Ben Williams and Grady Shelton <grady@>
2017-05-03 17:22:27 -0700

    pass two integration tests showing that a patron and a book can be added to the library

--- spec/library_system_integration_spec.rb
+++ spec/library_system_integration_spec.rb
@@ -1,17 +1,66 @@
+
 require "capybara/rspec"
 require "./app"
 
 Capybara.app = Sinatra::Application
 set(:show_exceptions, false)
 
-# example integration test
+describe('adding a new book', {:type => :feature}) do
+  it('allows a user to click a book list to add a book to the library') do
+    visit('/')
+    visit('/books')
+    visit('/book/add')
 
-# describe("the phrase parser path", {:type => :feature}) do
-#   it("processes the user input and returns correct message if its a palindrome") do
-#     visit("/")
-#     fill_in("phrase1", :with => "madam")
-#     fill_in("phrase2", :with => "anagram")
-#     click_button("what am i?")
-#     expect(page).to have_content("'madam' is a palindrome")
+    fill_in('title', :with =>'The Hobbit')
+    fill_in('authors', :with =>'Tolkien')
+
+    click_button('Add Book')
+    expect(page).to have_content('Book List')
+  end
+end
+
+describe('adding a new patron', {:type => :feature}) do
+  it('allows a user to click a book list to add a book to the library') do
+    visit('/')
+    visit('/patron_list')
+    visit('/patron/add')
+
+    fill_in('name', :with =>'Fred')
+
+    click_button('Add Patron')
+    expect(page).to have_content('Patron List')
+  end
+end
+
+# describe('viewing all of the lists', {:type => :feature}) do
+#   it('allows a user to see all of the lists that have been created') do
+#     list = List.new({:name => 'Epicodus Homework', :id => nil})
+#     list.save()
+#     visit('/')
+#     click_link('View All Lists')
+#     expect(page).to have_content(list.name)
 #   end
 # end
+#
+# describe('seeing details for a single list', {:type => :feature}) do
+#   it('allows a user to click a list to see the tasks and details for it') do
+#     test_list = List.new({:name => 'School stuff', :id => nil})
+#     test_list.save
+#     test_task = Task.new({:description => "learn SQL", :due_date => '2017-05-03 00:00:00', :list_id => test_list.id})
+#     test_task.save
+#     visit('/lists')
+#     click_link(test_list.name)
+#     expect(page).to have_content(test_task.description)
+#   end
+#
+#   describe('adding tasks to a list', {:type => :feature}) do
+#     it('allows a user to add a task to a list') do
+#       test_list = List.new({:name => 'School stuff', :id => nil})
+#       test_list.save
+#       visit("/lists/#{test_list.id}")
+#       fill_in("description", {:with => "Learn SQL"})
+#       fill_in("due_date", {:with => "2017-05-03 00:00:00"})
+#       click_button("Add task")
+#       expect(page).to have_content("Success")
+#     end
+#   end
